d477fd98758a9bbc5b2c6058b601fcae4cd4eabe
vijay-men0n <94514706+vijay-men0n@users.noreply.github.com>
2022-01-09 19:33:35 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="ADMINS PANEL">
+        <ol>
+            <li><a href="createuser.php">Create a user</a></li>
+            <li><a href="addnew.php">Add a Book to the List</a></li>
+            <li><a href="status.php">View Status of Books</a></li>
+            <li><a href="return.php">Return a Book</a></li>
+    </div>
+</body>
+</html>
